5de81cc8419b078dfe16a7ebd2863a2aaf197adb
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-08-31 22:37:48 +0700

    Post Foto Instagram

---
+++ src/components/PostIg.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PostIg() {
+  return (
+    <div className="flex flex-wrap mx-3">
+      <div className="w-1/3 p-px px-3">
+        <article class="flex mt-5 items-center">
+          <img src="/images/eginew.png" alt="" className="w-48" />
+          <img src="/images/eginew.png" alt="" className="w-48" />
+          <img src="/images/eginew.png" alt="" className="w-48" />
+        </article>
+      </div>
+    </div>
+  );
+}
