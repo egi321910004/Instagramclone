02f9f2728b06a986e0ed411ccefc3b0695145574
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-08-31 22:38:54 +0700

    Revisi Profile deskripsi

--- src/components/Profiledeskripsi.jsx
+++ src/components/Profiledeskripsi.jsx
@@ -13,7 +13,7 @@ export default function Profiledeskripsi() {
             alt="profile"
           />
           {/* Follower atau tentang profile */}
-          <div className="w-8/12 ml-28 mt-8 ">
+          <div className="w-8/12 ml-28 mt-8  ">
             <div className="mb-4">
               <h2 className="text-3xl inline-block font-light ">
                 egiramadani_
@@ -25,6 +25,23 @@ export default function Profiledeskripsi() {
                         sm:inline-block"
               >
                 Edit Profile
+              </button>
+              <button
+                className="p-1 ml-2 text-gray-700 border-transparent rounded-full  "
+                aria-label="Notifications"
+              >
+                <svg
+                  className="w-7 h-7 content-center  "
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                  <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
               </button>
             </div>
             <ul class="hidden md:flex space-x-8 mb-4">
