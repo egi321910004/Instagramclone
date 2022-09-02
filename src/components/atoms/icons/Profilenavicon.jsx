7615bacc1696c0a2563e70f17881915c523a04b4
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-02 12:19:06 +0700

    profilenavicon ke atom icons

---
+++ src/components/atoms/icons/Profilenavicon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Profilenavicon() {
+  return (
+    <div className="rounded-full overflow-hidden w-6 h-6 cursor-pointer ml-3 border-2 border-pink-600 ">
+      <img className="w-full" src="/images/eginew.png" alt="egi-rizki" />
+    </div>
+  );
+}
