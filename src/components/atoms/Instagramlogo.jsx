1e2035d5f8fb2ffb935d3192fe6dd736cc33559b
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-02 12:01:21 +0700

    logo instagram pindah ke atom

---
+++ src/components/atoms/Instagramlogo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Instagramlogo() {
+  return (
+    <div className="flex items-center">
+      <h1 className=" flex items-center">
+        <img
+          class="h-10 "
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/150px-Instagram_logo.svg.png"
+          alt="instagram"
+        />
+      </h1>
+    </div>
+  );
+}
