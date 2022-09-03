952fa8423d1938585612483de888b2a94136ec11
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 13:04:06 +0700

    Instagram foto to atoms

---
+++ src/components/atoms/Instagramfoto.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Instagramfoto() {
+  return (
+    <div>
+      <img
+        className="w-20 h-20 md:w-40 md:h-40 object-cover rounded-full
+                     border-2 border-pink-600 p-1 ml-32 mt-16"
+        src="/images/eginew.png"
+        alt="profile"
+      />
+    </div>
+  );
+}
