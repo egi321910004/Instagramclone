cfe6a78cd23b4f43f21cc7eced0e3a3a14d7b1e5
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 13:04:46 +0700

    follower profile to molekul

---
+++ src/components/molekuls/Follower.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Follower() {
+  return (
+    <div className="space-x-8 mb-4 flex items-center">
+      <li>
+        <span class="font-semibold mr-1">3</span>
+        posts
+      </li>
+
+      <li>
+        <span class="font-semibold mr-1">3,258</span>
+        followers
+      </li>
+      <li>
+        <span class="font-semibold mr-1">1,108</span>
+        following
+      </li>
+    </div>
+  );
+}
