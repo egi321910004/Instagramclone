d3bf0282524a101782771097747adaf40e602a59
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 12:44:11 +0700

    Post ig to organisms folder

---
+++ src/components/organisme/PostIg.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Gallerypost from "../molekuls/Gallerypost";
+
+export default function PostIg() {
+  return (
+    <div className="bg-gray-100 bg-opacity-25">
+      <Gallerypost />
+    </div>
+  );
+}
