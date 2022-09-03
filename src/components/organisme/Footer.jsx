169882df2a42f5ab20b46fc435615b2772e97328
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 13:05:12 +0700

    footer to organism

---
+++ src/components/organisme/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-gray-100 bg-opacity-25 ">
+      <span className="font-light text-stone-400 flex items-center justify-center">
+        Meta About Blog Jobs Help API Privacy Terms Top Accounts Hashtags
+        Locations Instagram Lite Contact Uploading & Non-Users
+      </span>
+      <div>
+        <p className="font-light text-stone-400 flex items-center justify-center mt-2 mb-2">
+          Indonesia © 2022 Instagram Clone by Egi Rizki
+        </p>
+      </div>
+    </div>
+  );
+}
