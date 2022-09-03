e981ff294be031d129c40bb4cc2c330a93795dfc
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 13:05:35 +0700

    refactoring src

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import FiturIg from "../components/organisme/FiturIg";
-import Footer from "../components/Footer";
+import Footer from "../components/organisme/Footer";
 import Navbar from "../components/organisme/Navbar";
 import PostIg from "../components/organisme/PostIg";
-import Profiledeskripsi from "../components/Profiledeskripsi";
+import Profiledeskripsi from "../components/organisme/Profiledeskripsi";
 
 export default function ProfilePage() {
   return (
