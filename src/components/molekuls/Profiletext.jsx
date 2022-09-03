43f72f70a54232f8ed212332e2960884c43dd25b
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-03 13:04:57 +0700

    profile text to molekul

---
+++ src/components/molekuls/Profiletext.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Profiletext() {
+  return (
+    <div className="hidden md:block">
+      <h1 class="font-semibold text-1xl">Egi Rizki Ramadani</h1>
+      <span>React and React Navtive</span>
+      <p className="text-blue-800">#Hacktiv8 #MSIB3</p>
+      <p>Quiz 4 </p>
+    </div>
+  );
+}
